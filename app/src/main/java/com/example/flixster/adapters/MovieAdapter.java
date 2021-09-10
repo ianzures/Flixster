cd65ad682a0fa22b69b58f4fbe83e69d82e10026
ianzures <i.anzures514@gmail.com>
2021-09-09 21:57:37 -0400

    Fixed issue with data not loading.

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,6 +1,8 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.flixster.R;
-import com.example.flixster.models.Movie;
+import com.example.flixter.models.Movie;
 
 import java.util.List;
 
@@ -21,21 +23,30 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     Context context;
     List<Movie> movies;
 
-    public MovieAdapter(Context context, List<Movie> movies){
+    public MovieAdapter(Context context, List<Movie> movies) {
         this.context = context;
         this.movies = movies;
     }
 
+
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        Log.d("MovieAdapter", "onCreateViewHolder");
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie,parent, false);
         return new ViewHolder(movieView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter", "onBindViewHolder " + position);
+
+        //get the movie at the passed in position
         Movie movie = movies.get(position);
+
+        // bind in the movie data into the view holder
+        //get data and populate the view holder with data of the movie
         holder.bind(movie);
     }
 
@@ -44,23 +55,30 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         return movies.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView tvTitle;
-        TextView tvOverView;
+        TextView tvOverview;
         ImageView ivPoster;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
-            tvOverView = itemView.findViewById(R.id.tvOverview);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
         }
 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
-            tvOverView.setText(movie.getOverView());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            tvOverview.setText(movie.getOverview());
+            String imageUrl;
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imageUrl = movie.getBackdropPath();
+            }
+            else {
+                imageUrl = movie.getPosterPath();
+            }
+            Glide.with(context).load(imageUrl).into(ivPoster);
         }
     }
 }
--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -1,4 +1,4 @@
-package com.example.flixster.models;
+package com.example.flixter.models;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -8,33 +8,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Movie {
+    String backdropPath;
     String posterPath;
     String title;
-    String overView;
+    String overview;
 
+
+    //take in json object and read out the fields we need
     public Movie(JSONObject jsonObject) throws JSONException {
+        backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
-        overView = jsonObject.getString("overview");
+        overview = jsonObject.getString("overview");
+
     }
 
-    public static List<Movie> fromJSONArray(JSONArray movieJsonArray) throws JSONException{
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
         List<Movie> movies = new ArrayList<>();
-        for(int i = 0; i < movieJsonArray.length(); i++){
+        for (int i = 0; i < movieJsonArray.length(); i++) {
             movies.add(new Movie(movieJsonArray.getJSONObject(i)));
         }
         return movies;
     }
 
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+        //%s is where the backdropPath goes
+    }
+
     public String getPosterPath() {
-        return String.format("https://image.tmdb.org/t/p/w342/%s",posterPath);
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+        //%s is where the posterPath goes
     }
 
     public String getTitle() {
         return title;
     }
 
-    public String getOverView() {
-        return overView;
+    public String getOverview() {
+        return overview;
     }
 }
