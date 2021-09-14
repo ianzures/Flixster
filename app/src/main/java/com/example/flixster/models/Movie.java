5081ab14b18f4c2febffdeb87917f0f306aa0976
ianzures <i.anzures514@gmail.com>
2021-09-14 15:29:59 -0400

    Youtube player now takes data from API.

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 @Parcel
 public class Movie {
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
@@ -22,6 +23,7 @@ public class Movie {
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public Movie(){}
@@ -54,5 +56,9 @@ public class Movie {
 
     public double getRating() {
         return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
